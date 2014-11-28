e4663213d7f0b15dd57b3066b1027d846adaa3af
Mark Thomas <markthethomas@gmail.com>
2014-11-28 11:20:21 -0800

    Change name error; update gitignore

--- instPy.py
+++
@@ -1,4 +0,0 @@
-from clint.textui import colored
-
-print colored.red('some warning message')
-print colored.green('nicely done!')
---
+++ instaPy.py
@@ -0,0 +1,4 @@
+from clint.textui import colored
+
+print(colored.red('some warning message'))
+print(colored.green('nicely done!'))
